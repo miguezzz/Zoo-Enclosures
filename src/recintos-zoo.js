58b8eadfecc47de0c775719479c61a5dd7fb2162
Victor Miguez <v.miguez2004@gmail.com>
2024-09-06 18:24:01 -0300

    feature: verifica se animal e quantidade são válidos

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,8 +1,49 @@
 class RecintosZoo {
 
+    constructor() {
+        this.recintos = [
+            // array com hash tables para representar a tabela recintos pq está categorizada com índice numérico
+            // em animais, um array com hash tables para representar possiveis outros animais no mesmo recinto
+            { numero: 1, biomas: ['savana'], tamanho_total: 10, animais: [{ especie: 'MACACO', quantidade: 3 }] },
+            { numero: 2, biomas: ['floresta'], tamanho_total: 5, animais: [] },
+            { numero: 3, biomas: ['savana', 'rio'], tamanho_total: 7, animais: [{ especie: 'GAZELA', quantidade: 1 }] },
+            { numero: 4, biomas: ['rio'], tamanho_total: 8, animais: [] },
+            { numero: 5, biomas: ['savana'], tamanho_total: 9, animais: [{ especie: 'LEAO', quantidade: 1 }] }
+        ];
+
+        this.animais = {
+            // objeto vai ficar melhor para representar a tabela de animais pq está categorizada por espécies
+            LEAO: 	{ tamanho: 3, biomas: ['savana'], carnivoro: true },
+            LEOPARDO: { tamanho: 2, biomas: ['savana'], carnivoro: true },
+            CROCODILO: { tamanho: 3, biomas: ['rio'], carnivoro: true },
+            MACACO: { tamanho: 1, biomas: ['savana', 'floresta'], carnivoro: false },
+            GAZELA: { tamanho: 2, biomas: ['savana'], carnivoro: false },
+            HIPOPOTAMO: { tamanho: 4, biomas: ['savana', 'rio'], carnivoro: false }
+        };
+    }
+    //  verifica se o animal existe na tabela dos animais
+    isAnimalValido(animal) {
+        return animal in this.animais;
+    }
+
+    // verifica se a quantidade de animais é um número e se é positivo
+    isQuantidadeValida(quantidade) {
+        return (Number.isInteger(quantidade) && quantidade > 0);
+    }
+
     analisaRecintos(animal, quantidade) {
+
+        if (!this.isAnimalValido(animal)) {
+            return { erro: "Animal inválido" };
+        }
+
+        if (!this.isQuantidadeValida(quantidade)) {
+            return { erro: "Quantidade inválida" };
+        }
     }
 
 }
 
 export { RecintosZoo as RecintosZoo };
+
+// new RecintosZoo().analisaRecintos('MACACO', 2);
